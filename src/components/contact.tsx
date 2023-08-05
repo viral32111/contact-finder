a4422070603da948dd00c53346d57a24fc6d728b
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-08-05 13:33:48 +0100

    Better contacts

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,13 +4,21 @@ import { faChevronUp } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useState } from "react"
 
+interface Address {
+	houseNumber: number,
+	streetName: string,
+	cityName: string,
+	countyName: string,
+	postCode: string
+}
+
 interface ContactAttributes {
 	pictureUrl: string,
 	fullName: string,
 	mobileNumber: string,
 	landlineNumber: string,
 	emailAddress: string,
-	homeAddress: string
+	homeAddress: Address
 }
 
 export const Contact: React.FC<ContactAttributes> = ( {
@@ -22,14 +30,15 @@ export const Contact: React.FC<ContactAttributes> = ( {
 	homeAddress
 } ) => {
 	const [ isCollapsed, setIsCollapsed ] = useState( true )
+	const [ isFavourited, setIsFavorited ] = useState( false )
 
-	return <div className="p-5 relative bg-white shadow-md">
+	return <div className="p-5 relative shadow-md bg-white text-gray-600">
 		<div className="flex items-center">
 			<img className="w-12 h-12 rounded-full mr-4" src={ pictureUrl } alt={ fullName } />
 
 			<div>
-				<h2 className="text-slate-900 text-xl">{ fullName }</h2>
-				<p className="text-gray-400 text-sm">{ mobileNumber }</p>
+				<h2 className="text-xl text-slate-700">{ fullName }</h2>
+				<p className="text-sm text-slate-400">{ mobileNumber }</p>
 			</div>
 
 			<button
@@ -40,5 +49,33 @@ export const Contact: React.FC<ContactAttributes> = ( {
 				<FontAwesomeIcon icon={ faChevronUp } size="2xl" />
 			</button>
 		</div>
+
+		{ !isCollapsed && (
+			<div className="mt-4 text-sm">
+				<hr className="mb-4" />
+
+				<div className="grid grid-cols-2 p-2">
+					<div>
+						<p className="font-bold">Address:</p>
+						<p>
+							{ homeAddress.houseNumber } { homeAddress.streetName },<br />
+							{ homeAddress.cityName },<br />
+							{ homeAddress.countyName },<br />
+							{ homeAddress.postCode }
+						</p>
+					</div>
+
+					<div>
+						<p className="font-bold">Landline: <span>{ landlineNumber }</span></p>
+						<p className="font-bold">Email: <span>{ emailAddress }</span></p>
+					</div>
+				</div>
+
+				<div className="flex justify-end items-center">
+					<label htmlFor="addToFavourites" className="mr-2 font-bold text-gray-400">Add to Favourites</label>
+					<input name="addToFavourites" id="addToFavourites" className="w-5 h-5" type="checkbox" checked={ isFavourited } onChange={ ( e ) => { setIsFavorited( e.target.checked ) } } />
+				</div>
+			</div>
+		) }
 	</div>
 }
--- src/components/container.tsx
+++ src/components/container.tsx
@@ -6,30 +6,54 @@ export const Container = () =>
 	<div className="py-10 px-16 space-y-5">
 		<Contact
 			pictureUrl="https://randomuser.me/api/portraits/men/90.jpg"
-			fullName="Joe Bloggs"
-			mobileNumber="+01 2345 6789"
-			landlineNumber="+98 7654 3210"
-			emailAddress="joe@bloggs.com"
-			homeAddress="123 Example Street, City, County, AB12 9XY" />
+			fullName="Terry Bloggs"
+			mobileNumber="0741-713-263"
+			landlineNumber="015394 92955"
+			emailAddress="joe.bloggs@example.com"
+			homeAddress={ {
+				houseNumber: 384,
+				streetName: "George Street",
+				cityName: "Leicester",
+				countyName: "Essex",
+				postCode: "H52 1FE"
+			} } />
 		<Contact
 			pictureUrl="https://randomuser.me/api/portraits/men/90.jpg"
-			fullName="Joe Bloggs"
-			mobileNumber="+01 2345 6789"
-			landlineNumber="+98 7654 3210"
-			emailAddress="joe@bloggs.com"
-			homeAddress="123 Example Street, City, County, AB12 9XY" />
+			fullName="Terry Bloggs"
+			mobileNumber="0741-713-263"
+			landlineNumber="015394 92955"
+			emailAddress="joe.bloggs@example.com"
+			homeAddress={ {
+				houseNumber: 384,
+				streetName: "George Street",
+				cityName: "Leicester",
+				countyName: "Essex",
+				postCode: "H52 1FE"
+			} } />
 		<Contact
 			pictureUrl="https://randomuser.me/api/portraits/men/90.jpg"
-			fullName="Joe Bloggs"
-			mobileNumber="+01 2345 6789"
-			landlineNumber="+98 7654 3210"
-			emailAddress="joe@bloggs.com"
-			homeAddress="123 Example Street, City, County, AB12 9XY" />
+			fullName="Terry Bloggs"
+			mobileNumber="0741-713-263"
+			landlineNumber="015394 92955"
+			emailAddress="joe.bloggs@example.com"
+			homeAddress={ {
+				houseNumber: 384,
+				streetName: "George Street",
+				cityName: "Leicester",
+				countyName: "Essex",
+				postCode: "H52 1FE"
+			} } />
 		<Contact
 			pictureUrl="https://randomuser.me/api/portraits/men/90.jpg"
-			fullName="Joe Bloggs"
-			mobileNumber="+01 2345 6789"
-			landlineNumber="+98 7654 3210"
-			emailAddress="joe@bloggs.com"
-			homeAddress="123 Example Street, City, County, AB12 9XY" />
+			fullName="Terry Bloggs"
+			mobileNumber="0741-713-263"
+			landlineNumber="015394 92955"
+			emailAddress="joe.bloggs@example.com"
+			homeAddress={ {
+				houseNumber: 384,
+				streetName: "George Street",
+				cityName: "Leicester",
+				countyName: "Essex",
+				postCode: "H52 1FE"
+			} } />
 	</div>
