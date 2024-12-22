ba82a18eeaae69b3894ac3359b032df1ece79e46
viral32111 <19510403+viral32111@users.noreply.github.com>
2024-12-22 11:42:37 +0000

    Fix assets not importing on GitHub Pages due to leading slashes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig( {
+	base: "", // Remove leading slash from assets - https://stackoverflow.com/a/78204770
 	plugins: [ react() ],
 } )
